448253e0052f2dfaf984e36010c7fb91a7314e11
Siobhantt <luisa.cardozo@iesnervion.es>
2023-02-15 18:46:59 +0100

    Terminado segundo ejercicio

--- BoletinT5/src/sieteMedia/FuncionesSieteMedia.java
+++ BoletinT5/src/sieteMedia/FuncionesSieteMedia.java
@@ -4,112 +4,119 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class FuncionesSieteMedia {
+
 	static Scanner lee = new Scanner(System.in);
 
+	// Lo primero que necesito es mostrar un mensaje de bienvenida al juego
+	public static void bienvenida() {
+		System.out.println("Bienvenido al Siete y medio!");
+	}
 
-	//creo una funcion que sea la nueva partida y esta va a inicializar el array a 0
-	//recibe una tabla
-	public static void nuevaPartida(double [] jugadores) {
+	// creo una funcion que sea la nueva partida y esta va a inicializar el array a
+	// 0
+	// recibe una tabla
+	public static void nuevaPartida(double[] jugadores) {
 		Arrays.fill(jugadores, 0);
 	}
-	//funcion que devuelve el resultado almacenadoç
-	//recibe el jugador
 
-	//creo una funcion que sea la ronda y esta recibirá el jugador correspondiente y la tabla
-	//dentro haré un bucle que se mantendra activo hasta que 
-	//quiero que esta funcion mientras el user me de la oopcion si siga dando cartas, hasta que diga que no o la puntuacion
-	//sea mayor de 7.5
-	public static void tirada(double [] t, int jugador) {
+	/*
+	 * La funcion tirada recibe la tabla donde estan las puntuaciones y un entero
+	 * que representa al jugador
+	 */
+	public static void tirada(double[] t, int jugador) {
 		int carta;
 		String respuesta;
-		respuesta=pregunta();
-		while((respuesta.equals("s")||respuesta.equals("S")) && t[jugador]<= 7.5) {
+		// uso la funcion pregunta para obtenerla del jugador
+		respuesta = pregunta();
+		/*
+		 * Inicia un bucle while donde si la respuesta es igual a s,S Y la puntuacion es
+		 * menor que 7,5 se mantenga activo
+		 */
+		while ((respuesta.equals("s") || respuesta.equals("S")) && t[jugador] <= 7.5) {
+			// dentro del bucle llamo a la funcion que me da la carta aleatoria
 			carta = cartAleatoria();
-			if(carta == 8) {
+			// este bloque es para el caso de que sea 7, 8 o 9, en esos cassos a cada uno le
+			// asigné sota caballo y rey.
+			if (carta == 8) {
 				System.out.println("Usted ha sacado la carta Sota de " + palo());
-			}
-			else if(carta == 9) {
+			} else if (carta == 9) {
 				System.out.println("Usted ha sacado la carta Caballo de " + palo());
-			}
-			else if(carta == 10) {
+			} else if (carta == 10) {
 				System.out.println("Usted ha sacado la carta Rey de " + palo());
-			}
+			} // a todo lo deas se asigné un palo con una funcion
 			else {
-				System.out.println("Usted ha sacado la carta "+carta + " de " + palo());
-
+				System.out.println("Usted ha sacado la carta " + carta + " de " + palo());
 			}
-			t[jugador]=sumaPuntuacion(t, carta, jugador);
-			System.out.println("Actualmente tiene "+t[jugador]+" puntos");
-			System.out.println();
-			if(t[jugador]<= 7.5) {
-			respuesta=pregunta();
+			// una vez generado la carta con su palo correspondiente guardo en la tabla la
+			// suma de la puntuacion con la funcion correspondiente
+			t[jugador] = sumaPuntuacion(t, carta, jugador);
+			// informacion sobre la puntuacion que tiene el jugador
+			System.out.println("Actualmente tiene " + t[jugador] + " puntos");
+			System.out.println(); //
+			// si el jugador sigue teniendo menos de 7.5 sigo preguntando
+			if (t[jugador] <= 7.5) {
+				respuesta = pregunta();
 			}
-		}
+		} // finn del while
 	}
-	//funcion de pregunta
-	
+
+	// funcion de pregunta que recibe la opcion del user
 	public static String pregunta() {
 		String opcionUser;
 		System.out.println("Quiere sacar una carta? (s/n)");
 		opcionUser = lee.next();
-	/*	if (opcionUser != "s"  && opcionUser!="n") {
-			opcionUser = lee.next();
-		}*/
 		return opcionUser;
 	}
-//Lo primero que necesito es mostrar un mensaje de bienvenida al juego
-	public static void bienvenida() {
-		System.out.println("Bienvenido al Siete y medio!");
-	}
-	
- // luego muestro que es el turno del jugador 1 y
+/*Como la puntuacion de cada jugador esta guardado en una tabla con las posiciones 0 y 1, le sumo 1 al jugador para que se califique como 1 y 2*/
 	public static String turno(int jugador) {
-		return "Es el turno del jugador "+ (jugador+1);
-	} 
-	
- // devuelve una carta al azar
+		return "Es el turno del jugador " + (jugador + 1);
+	}
+
+	// devuelve una carta al azar entre 1 y 10 
 	public static int cartAleatoria() {
-		return (int) ((Math.random()*10)+1);
+		return (int) ((Math.random() * 10) + 1);
 	}
-	
+//esta funcion me ofrece un palo al azar
+	//guardo un numero random en la variable azar entre 1 y 4 entonces tomará uno de el array que contiene los palos y lo devolverá
 	public static String palo() {
-	String palo[] = {"basto" , "oro" , "copas", "espadas"};
-	int azar = (int)(Math.random()*4);
-	return palo[azar];
+		String palo[] = { "basto", "oro", "copas", "espadas" };
+		int azar = (int) (Math.random() * 4);
+		return palo[azar];
 	}
-	
-// tengo que definir la puntuacion que será la suma de las cartas que se iran sacando
-	public static double sumaPuntuacion(double []t, int carta, int jugador) {
-		if(carta>=1&&carta<=7) {
+
+//la suma de las cartas que se iran sacando, mientras esten entro 1 y 7 se sumaran a la puntuacion, todo lo demas valdrá 0.5 y eso(0,5) será lo que se sume a la puntuacion
+	public static double sumaPuntuacion(double[] t, int carta, int jugador) {
+		if (carta >= 1 && carta <= 7) {
 			t[jugador] += carta;
-		}else {
-			t[jugador]+=0.5;
+		} else {
+			t[jugador] += 0.5;
 		}
 		return t[jugador];
 	}
-// genero una carta aleatoria, y mantengo generando una carta aleatoria hasta que me diga que no o la puntuacion pase de 7.5
-// en caso que me diga que no termina el turno del  jugador 1 y paso al jugador 2,
- 
+	/*Funcion que guarda los casos de cuando se gana
+	 * en ella esta el caso de si ambos jugadores se pasan, 
+	 * si solo se pasa el primer jugador
+	 * si solo se pasa el segundo jugador
+	 * y si ambos jugadores terminan con la misma puntuacion
+	 * devuelve el jugador.*/
 	public static String ganador(double[] t, int jugador1, int jugador2) {
-		String ganador=null;
-		if(t[jugador1]>7.5&&t[jugador2]>7.5) {
-			ganador="Ambos jugadores han perdido.";
-		}else if(t[jugador1]>7.5) {
-			ganador="Gana jugador 2";
-		}else if(t[jugador2]>7.5) {
-			ganador="Gana jugador 1";
-		}else {
-			if(t[jugador1]==t[jugador2]) {
-				ganador="Empate";
-			}else if(t[jugador1]>t[jugador2]) {
-				ganador="Gana el jugador 1";
-			}else if(t[jugador1]<t[jugador2]) {
-				ganador="Gana el jugador 2";
+		String ganador = null;
+		if (t[jugador1] > 7.5 && t[jugador2] > 7.5) {
+			ganador = "Ambos jugadores han perdido.";
+		} else if (t[jugador1] > 7.5) {
+			ganador = "Gana jugador 2";
+		} else if (t[jugador2] > 7.5) {
+			ganador = "Gana jugador 1";
+		} else {
+			if (t[jugador1] == t[jugador2]) {
+				ganador = "Empate";
+			} else if (t[jugador1] > t[jugador2]) {
+				ganador = "Gana el jugador 1";
+			} else if (t[jugador1] < t[jugador2]) {
+				ganador = "Gana el jugador 2";
 			}
 		}
-
 		return ganador;
 	}
- 
- 
+
 }
--- BoletinT5/src/sieteMedia/MainSieteMedia.java
+++ BoletinT5/src/sieteMedia/MainSieteMedia.java
@@ -1,14 +1,15 @@
 package sieteMedia;
 
 public class MainSieteMedia {
-	//declaro que el jugador 1 es la posicion 0 del array
-		final static int JUGADOR1 =0;
-		//y que el jugador 2 es la posicion 1 del array
-		final static int JUGADOR2 =1;
-		
+	// declaro que el jugador 1 es la posicion 0 del array
+	final static int JUGADOR1 = 0;
+	// y que el jugador 2 es la posicion 1 del array
+	final static int JUGADOR2 = 1;
+
 	public static void main(String[] args) {
+
 		double jugadores[] = new double[2];
-		
+
 		FuncionesSieteMedia.nuevaPartida(jugadores);
 		FuncionesSieteMedia.bienvenida();
 		System.out.println();
@@ -16,7 +17,7 @@ public class MainSieteMedia {
 		FuncionesSieteMedia.tirada(jugadores, JUGADOR1);
 		System.out.println();
 		System.out.println(FuncionesSieteMedia.turno(JUGADOR2));
-		FuncionesSieteMedia.tirada(jugadores,JUGADOR2);
+		FuncionesSieteMedia.tirada(jugadores, JUGADOR2);
 		System.out.println();
 		System.out.println(FuncionesSieteMedia.ganador(jugadores, JUGADOR1, JUGADOR2));
 	}
